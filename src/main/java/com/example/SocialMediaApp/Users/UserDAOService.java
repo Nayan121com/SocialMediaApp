0b20951c64c35683cfe8ab307d843486725f9275
Nayan Gupta <kanumgupta32@gmail.com>
2024-04-14 20:58:17 +0530

    Implemented User Data Access Object.

--- src/main/java/com/example/SocialMediaApp/Users/UserDAOService.java
+++ src/main/java/com/example/SocialMediaApp/Users/UserDAOService.java
@@ -1,26 +1,42 @@
 package com.example.SocialMediaApp.Users;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class UserDAOService {
 
-    private static List<User> userList = new ArrayList<>();
+    private static Map<Integer, User> userList = new HashMap<>();
     private static int userCount = 0;
 
     static {
-        userList.add(new User(userCount++, "Nayan Gupta", LocalDateTime.now()));
-        userList.add(new User(userCount++, "Rounak Kumar", LocalDateTime.now()));
-        userList.add(new User(userCount++, "Aditya Vashist", LocalDateTime.now()));
-        userList.add(new User(userCount++, "Uday Kumar", LocalDateTime.now()));
-        userList.add(new User(userCount++, "Dhruv Mehta", LocalDateTime.now()));
-        userList.add(new User(userCount++, "Yash Pandya", LocalDateTime.now()));
+        userList.put(++userCount, new User(userCount, "Nayan Gupta", LocalDateTime.now()));
+        userList.put(++userCount, new User(userCount, "Rounak Kumar", LocalDateTime.now()));
+        userList.put(++userCount, new User(userCount, "Aditya Vashist", LocalDateTime.now()));
+        userList.put(++userCount, new User(userCount, "Uday Kumar", LocalDateTime.now()));
+        userList.put(++userCount, new User(userCount, "Dhruv Mehta", LocalDateTime.now()));
+        userList.put(++userCount, new User(userCount, "Yash Pandya", LocalDateTime.now()));
     }
 
-    //TODO: save user
-    //TODO: FetchAllUsers
-    //TODO: findOne User
-    //TODO: Delete User
-
+    //save user
+    public User save(@org.jetbrains.annotations.NotNull User user){
+        user.setId(++userCount);
+        userList.put(userCount, user);
+        return user;
+    }
+    //FetchAllUser
+    public List<User> fetchAllUser(){
+        List<User> allUsers = new ArrayList<>();
+        for(Map.Entry<Integer, User> user : userList.entrySet()){
+            allUsers.add(user.getValue());
+        }
+        return allUsers;
+    }
+    //findOne User
+    public User findUser(int id){
+        return userList.getOrDefault(id, null);
+    }
+    //Delete User
+    public void deleteUser(int id){
+        userList.remove(id);
+    }
 }
